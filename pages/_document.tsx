d6dbe0deff1349315cb141f2a5d69aaaa839d8ea
HWANBINYOO <dngh0825@naver.com>
2023-03-13 08:31:41 +0900

    Fix : Head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head >
-      <meta 
-        httpEquiv="Content-Security-Policy" 
-        content="upgrade-insecure-requests" 
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
