50b9ea54aa87e86d2ce525d45ba0f1fefdb9d653
HWANBINYOO <dngh0825@naver.com>
2023-03-29 22:32:53 +0900

    Add storybook

---
+++ stories/pages/home.stories.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import PostPage from "../../pages/post/index";
+
+export default {
+  title: "Pages/Post",
+  component: Post,
+};
+
+export const Post = () => <PostPage />;
